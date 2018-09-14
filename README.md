77f94841c9bcdb3b9e99ff7be59752fb55a1d690
marwan-nabil <marwan.nabil.mohammed@gmail.com>
2018-09-14 10:43:04 +0200

    finished chapter 1

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # k and r
-solved exercises, variations of code segments and programs inspired by the book "the c programming language"
+solved exercises, variations of code segments and programs,   inspired by the book "the c programming language".
+
+## [Chapter 1](./chapter_1)
+[comments_remover.c](./chapter_1/comments_remover.c) : removes all c-style comments from input files.
+[detab.c](./chapter_1/detab.c) : replaces all tabs with 4 spaces.
+[entab.c](./chapter_1/entab.c) : replaces all 4 consecutive spaces with a tab.
+[fold_lines.c](./chapter_1/fold_lines.c) : folds long input lines into limited length lines.
+[program_checker.c](./chapter_1/program_checker.c) : checks a program for mismatched parens syntax errors.
 
--- chapter_1/program_checker.c
+++ chapter_1/program_checker.c
@@ -1,7 +1,86 @@
 /*
 the program should check a c source file for unmatched symbols, 
 like parantheses or braces.
+also should check for arbitrary syntax errors.
 */
-
-
 #include<stdio.h>
+#include<string.h>
+
+#define STACKMAX 30
+char stack[STACKMAX];
+char *stack_p = stack;
+
+void push(char c){
+  *stack_p++ = c;
+}
+char pop(){
+  return *(--stack_p);
+}
+char peek(int i){
+  return *(stack_p - i);
+}
+
+
+char *left_paren =  "<({[";
+char *right_paren = ">)}]";
+
+typedef enum {LEFT, RIGHT, NOT_PAREN} paren_type_t;
+typedef enum {UNKNOWN, CURLYS, BRACES, BRACKS, ANGLED} paren_class_t;
+
+paren_type_t what_paren_type(char s)
+{
+  if(strchr(left_paren, s))
+    return LEFT;
+  else if(strchr(right_paren, s))
+    return RIGHT;
+  else
+    return NOT_PAREN;
+}
+
+paren_class_t what_paren_class(char c)
+{
+  if(c=='<' || c=='>')
+    return ANGLED;
+  else if(c == '[' || c==']')
+    return BRACKS;
+  else if(c=='(' || c==')')
+    return BRACES;
+  else if(c=='{' || c=='}')
+    return CURLYS;
+  else
+    return UNKNOWN;
+}
+
+void check_program(void)
+{
+  char c;
+  paren_type_t t;
+  paren_class_t cl;
+
+  while((c = getchar())!= EOF){
+    t = what_paren_type(c);
+
+    if(t == NOT_PAREN)
+      printf("%c",c);
+    else if(t == LEFT){
+      push(c);
+      printf("%c",c);
+    }
+    else if(t == RIGHT){
+      if(what_paren_class(c) != what_paren_class(peek(1))){
+	printf("%c",c);
+	printf("  << Syntax error: unmatched parens\n");
+	return ;
+      }
+      else{
+	printf("%c",c);
+	pop();
+      }
+    }
+  }
+}
+
+int main()
+{
+  check_program();
+}
